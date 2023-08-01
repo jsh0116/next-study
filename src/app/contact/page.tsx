63c5f8e6045c3bf09b89aed35779b0b86f8f19a4
jsh0116 <seunghoon7561@gmail.com>
2023-08-02 01:27:26 +0900

    [feat] static routing 적용
    - next 13 버전에 따른 static routing 적용

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react';
+
+const Page = () => {
+  return (
+    <h1>ContactPage</h1>
+  );
+};
+
+export default Page;
---
+++ src/app/product/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <h1>product</h1>
+  );
+};
+
+export default Page;
